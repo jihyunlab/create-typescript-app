79f61b66a22b4346cac4777606ff613bcd8269fa
jihyunlab-phil <philjung.yun@jihyunlab.com>
2024-06-29 21:18:41 +0900

    improve design

--- test/creators/project-creator.service.test.ts
+++ test/creators/project-creator.service.test.ts
@@ -1,4 +1,4 @@
-import { ProjectCreator } from '../../bin/creators/project.creator';
+import { ProjectCreator } from '../../bin/creators/project-creator.service';
 import { join } from 'path';
 import { mkdirSync, readFileSync, rmSync } from 'fs';
 
